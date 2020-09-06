74c11a1534d37c1d7fc5327803391ba533d865f1
Alex Anoshkin <Anoshkin.allex@gmail.com>
2020-09-06 16:43:29 +0300

    integrate UserController with StatisticService

--- src/main/java/com/example/testing_aop_proxy/controller/UserController.java
+++ src/main/java/com/example/testing_aop_proxy/controller/UserController.java
@@ -1,32 +1,61 @@
 package com.example.testing_aop_proxy.controller;
 
 import com.example.testing_aop_proxy.dto.User;
+import com.example.testing_aop_proxy.entity.Statistic;
+import com.example.testing_aop_proxy.service.StatisticService;
 import com.example.testing_aop_proxy.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-public class UserController{
+public class UserController {
 
     private final UserService userService;
+    private final StatisticService statisticService;
+    private static final String CLASS_NAME = "UserController";
 
-    public UserController(UserService userService) {
+
+    public UserController(UserService userService, StatisticService statisticService) {
         this.userService = userService;
+        this.statisticService = statisticService;
     }
 
     @GetMapping("/users")
     public List<User> getUsers() {
-       return userService.getUsers();
+        long startedTime = System.currentTimeMillis();
+        List<User> users =  userService.getUsers();
+        statisticService.save(getStatistic("getUsers", startedTime, null));
+        return users;
     }
+
+
 
     @GetMapping("/users/{id}")
     public User getUser(@PathVariable("id") String id) {
-        return userService.getUser(id);
+        long startedTime = System.currentTimeMillis();
+        User user =  userService.getUser(id);
+
+        statisticService.save(getStatistic("getUser", startedTime, id));
+
+        return user;
     }
 
-    @PostMapping("/users/{id}")
-    public String getUser(@RequestBody User user) {
-        return userService.createUser(user);
+    @PostMapping("/users")
+    public String createUser(@RequestBody User user) {
+        long startedTime = System.currentTimeMillis();
+        String id =  userService.createUser(user);
+        statisticService.save(getStatistic("createUser", startedTime, id));
+        return id;
+    }
+
+    private Statistic getStatistic(String method, long start, String requestId) {
+        return Statistic.builder()
+                .className(CLASS_NAME)
+                .method(method)
+                .duration(System.currentTimeMillis() - start)
+                .requestId(requestId)
+                .created(System.currentTimeMillis())
+                .build();
     }
 }
