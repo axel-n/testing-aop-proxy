55321158b28fbb929cd15f480a48a48b3b0865dd
Alex Anoshkin <Anoshkin.allex@gmail.com>
2020-09-06 17:31:52 +0300

    fix save data with batch

--- src/main/java/com/example/testing_aop_proxy/entity/Statistic.java
+++ src/main/java/com/example/testing_aop_proxy/entity/Statistic.java
@@ -10,7 +10,7 @@ import javax.persistence.*;
 public class Statistic {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.TABLE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(nullable = false)
